0c4e58a7d08439f9b19faad717bbd443e07fb508
Hongseok Kim <redstone@utexas.edu>
2025-02-19 15:56:24 +0900

    021925 update

--- RS_WISP_05_RPOD_Functions/eci2lvlh.m
+++ RS_WISP_05_RPOD_Functions/eci2lvlh.m
@@ -2,8 +2,8 @@ function delta_r_LVLH = eci2lvlh(delta_r_eci, r_eci, v_eci)
   
     % Reference Frame of the Target
     i_hat = r_eci/norm(r_eci);
-    j_hat = v_eci/norm(v_eci);
-    k_hat = cross(i_hat, j_hat);
+    k_hat = cross(r_eci, v_eci) / norm(cross(r_eci, v_eci));
+    j_hat = cross(k_hat,i_hat);
     
     % Transformation Matrix from ECI to Space Station Frame
     Q_LVLH_ECI = [i_hat';j_hat';k_hat'];
